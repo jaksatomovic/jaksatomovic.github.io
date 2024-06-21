fec935437a9a2cf2031488491cdf1c6525a87c6d
canarin <jaksa.tomovic@gmail.com>
2024-06-21 09:47:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
+   site: 'https://jaksatomovic.github.io',
+   base: 'jaksatomovic.github.io',
   integrations: [tailwind(),  sitemap()]
 });
