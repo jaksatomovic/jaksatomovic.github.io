4a6ee988edce8f704e8e46848211c2c46fdda045
canarin <jaksa.tomovic@gmail.com>
2024-06-21 09:48:00 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
    site: 'https://jaksatomovic.github.io',
-   base: 'jaksatomovic.github.io',
   integrations: [tailwind(),  sitemap()]
 });
